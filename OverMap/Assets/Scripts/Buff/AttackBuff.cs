bb9bff29205daae730d52448db95946c3535ee9b
liuzhongxiu <1025466218@qq.com>
2020-07-14 09:12:55 +0800

    buff系统漏传配置

---
+++ OverMap/Assets/Scripts/Buff/AttackBuff.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Assets.Scripts.Buff
+{
+    class AttackBuff : BuffBase
+    {
+
+
+        public override void OnEnter()
+        {
+            base.OnEnter();
+            Debug.Log("获得攻击buff");
+            HexGameUI.Instrance.selectedUnit.unitBase.attack += 1;
+        }
+    }
+}
---
+++ OverMap/Assets/Scripts/Buff/BuffBase.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Assets.Scripts.Buff
+{
+
+    public enum Bufftype
+    {
+        None = 0,
+        Attack = 1,
+        Defend = 2,
+        RecoverBlood = 3,
+    }
+
+    public interface IBuff
+    {
+        int GetID();
+        void OnEnter();
+        void OnUpdate();
+        void OnExit();
+    }
+
+    public class BuffBase : IBuff
+    {
+        public delegate void EnterDel(HexUnit unit);
+
+        public delegate void UpdateDel();
+
+        public delegate void ExitDel();
+
+        public delegate void ReturnDel();
+
+        public object[] parms; //参数列表
+        private bool canUpdate;
+        private int id;
+        private bool canDestroy;
+
+        public Bufftype bufftype = Bufftype.None;
+
+        public BuffBase()
+        {
+
+        }
+
+        public void SetDel()
+        {
+
+        }
+
+        public void SwitchUpdate(bool canUpdate)
+        {
+            this.canUpdate = canUpdate;
+        }
+
+        public int GetID()
+        {
+            return id;
+        }
+
+        public virtual void OnEnter()
+        {
+
+        }
+
+        public void OnUpdate()
+        {
+
+        }
+
+        public void OnExit()
+        {
+
+        }
+    }
+}
