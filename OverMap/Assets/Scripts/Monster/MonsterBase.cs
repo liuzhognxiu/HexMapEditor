dccbe3f0dc96e942332a29324c2fd5fd7ece697c
liuzhongxiu <1025466218@qq.com>
2020-07-21 14:22:43 +0800

    如果怪物死亡之后，当前没有清理，添加血量判断

--- OverMap/Assets/Scripts/Monster/MonsterBase.cs
+++ OverMap/Assets/Scripts/Monster/MonsterBase.cs
@@ -5,9 +5,6 @@ namespace Assets.Scripts.Monster
 {
     public class MonsterBase : HexUnit
     {
-
-        public GameObject monster;
-
         public void Start()
         {
             isCanSelect = false;
@@ -17,15 +14,18 @@ namespace Assets.Scripts.Monster
                 attack = 1,
                 speed = 0,
                 defend = 3,
-                unit  = this,
+                unit = this,
             };
         }
 
         public virtual void FindHeroCell()
         {
+            if (this.unitBase.hp > 0)
+            {
+                Grid.FindPath(this.Location, HexGameUI.Instrance.selectedUnit.Location.GetNeighbor(HexDirection.E), this);
+                this.Travel(Grid.GetPath());
+            }
 
-            Grid.FindPath(this.Location, HexGameUI.Instrance.selectedUnit.Location.GetNeighbor(HexDirection.E), this);
-            this.Travel(Grid.GetPath());
         }
     }
 }
