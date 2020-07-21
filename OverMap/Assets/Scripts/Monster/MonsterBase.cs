c81be223b2727748b63f2dd5d31401e774c63fd6
liuzhongxiu <1025466218@qq.com>
2020-07-21 17:13:06 +0800

    添加获取怪物在玩家的什么方向上的代码

--- OverMap/Assets/Scripts/Monster/MonsterBase.cs
+++ OverMap/Assets/Scripts/Monster/MonsterBase.cs
@@ -34,6 +34,12 @@ namespace Assets.Scripts.Monster
         HexCell GetToCell(HexCell cell)
         {
             HexCell toCell = new HexCell();
+            HexDirection hexDirection = GetHexDirection(cell);
+            if (cell.GetNeighbor(hexDirection) != null && cell.GetNeighbor(hexDirection).Unit == null)
+            {
+                return cell.GetNeighbor(hexDirection);
+            }
+
             for (int i = 0; i < (int)HexDirection.NW; i++)
             {
                 if (cell.GetNeighbor((HexDirection)i) != null && cell.GetNeighbor((HexDirection)i).Unit == null)
@@ -47,6 +53,57 @@ namespace Assets.Scripts.Monster
                 toCell = GetToCell(HexGameUI.Instrance.selectedUnit.Location.GetNeighbor(HexDirection.NE));
             }
             return toCell;
+        }
+
+        /// <summary>
+        /// 判断怪物在玩家的什么方向上
+        /// </summary>
+        /// <param name="cell"></param>
+        /// <returns></returns>
+        HexDirection GetHexDirection(HexCell cell)
+        {
+            if (cell.coordinates.X == HexGameUI.Instrance.selectedUnit.Location.coordinates.X)
+            {
+                if (cell.coordinates.Y > HexGameUI.Instrance.selectedUnit.Location.coordinates.Y)
+                {
+                    return HexDirection.NE;
+                }
+                else
+                {
+                    return HexDirection.SW;
+                }
+            }
+            if (cell.coordinates.X < HexGameUI.Instrance.selectedUnit.Location.coordinates.X)
+            {
+                if (cell.coordinates.Z == HexGameUI.Instrance.selectedUnit.Location.coordinates.Z)
+                {
+                    return HexDirection.W;
+                }
+                else if (cell.coordinates.Z > HexGameUI.Instrance.selectedUnit.Location.coordinates.Z)
+                {
+                    return HexDirection.NW;
+                }
+                else
+                {
+                    return HexDirection.SW;
+                }
+            }
+            if (cell.coordinates.X > HexGameUI.Instrance.selectedUnit.Location.coordinates.X)
+            {
+                if (cell.coordinates.Z == HexGameUI.Instrance.selectedUnit.Location.coordinates.Z)
+                {
+                    return HexDirection.E;
+                }
+                else if (cell.coordinates.Z > HexGameUI.Instrance.selectedUnit.Location.coordinates.Z)
+                {
+                    return HexDirection.NE;
+                }
+                else
+                {
+                    return HexDirection.SE;
+                }
+            }
+            return HexDirection.NE;
         }
     }
 }
